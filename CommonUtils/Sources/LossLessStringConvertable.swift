5f3ac6babb8efbda0c26df10c357cdb80ab1d71b
hari <hariprasath.dp@zohocorp.com>
2025-12-02 16:18:56 +0530

    updated some thing

---
+++ CommonUtils/Sources/LossLessStringConvertable.swift
@@ -0,0 +1,16 @@
+//
+//  LossLessStringConvertable.swift
+//  Libs
+//
+//  Created by Hari on 02/12/25.
+//
+
+import Foundation
+
+extension LosslessStringConvertible {
+    
+    var string: String {
+        
+        String(self)
+    }
+}
